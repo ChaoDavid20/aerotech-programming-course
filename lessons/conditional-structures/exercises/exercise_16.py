2ba7fd2c3d2aea1ec5ff3fff005bd0786b497aec
ChaoDavid20 <chaodavid20@gmail.com>
2023-10-25 17:40:24 +0200

    Update exercise_16.py

--- lessons/conditional-structures/exercises/exercise_16.py
+++ lessons/conditional-structures/exercises/exercise_16.py
@@ -1,1 +1,11 @@
 n = 2  # Do not modify this line.
+if n < 0:
+    message = "The number is negative"
+if n > 0:
+    message = "The number is positive"
+if n == 0:
+    message = "The number is zero"
+if (n % 2) == 0:
+    message = message + " " + "and even"
+if (n % 2) != 0:
+    message = message + " " + "and odd"
