a9394a0615f68c5389f95370b90c19458b084ff9
ChaoDavid20 <chaodavid20@gmail.com>
2023-10-31 01:01:00 +0100

    Update exercise_32.py

--- lessons/libraries/exercises/exercise_32.py
+++ lessons/libraries/exercises/exercise_32.py
@@ -6,13 +6,11 @@ def splitter():
     R = img.copy()
     G = img.copy()
     B = img.copy()
-    for i in range(768):
-        for j in range(1024):
-            R[i][j][1] = 0
-            R[i][j][2] = 0
-            G[i][j][0] = 0
-            G[i][j][2] = 0
-            B[i][j][1] = 0
-            B[i][j][0] = 0
+    R[:, :, 1] = 0
+    R[:, :, 2] = 0
+    G[:, :, 0] = 0
+    G[:, :, 2] = 0
+    B[:, :, 1] = 0
+    B[:, :, 0] = 0
 
     return [R, G, B]
