027d7ee73a45aa70ca82ee8907c006aa1fde30e8
ChaoDavid20 <chaodavid20@gmail.com>
2023-11-04 19:52:42 +0100

    Update exercise_25.py
    Linters check

--- lessons/exceptions/exercises/exercise_25.py
+++ lessons/exceptions/exercises/exercise_25.py
@@ -1,6 +1,22 @@
 def weird_number(x):
+    """Multiple operations with a value.
+
+    This function performs multiple operations with a given value. First, tries
+    to cast the value to a float. If it does not work, sets the value to `0`.
+    If it works, sets the value its cube (power). After, adds one to the value.
+    Finally, returns the value.
+
+    Args:_
+        x: the original value.
+
+    Returns_
+        x: the final value.
+
+    """
     if not isinstance(x, (float, int, str)):
-        raise TypeError("this function only accepts integers, floats and strings.")
+        raise TypeError(
+            "this function only accepts integers, floats and strings."
+        )
 
     try:
         x = float(x)
