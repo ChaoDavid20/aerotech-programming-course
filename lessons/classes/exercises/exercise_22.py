644b073884fe902914ba5260364bba824d3ac8f4
ChaoDavid20 <chaodavid20@gmail.com>
2023-11-04 19:40:20 +0100

    Update exercises 22-24
    Linters check

--- lessons/classes/exercises/exercise_22.py
+++ lessons/classes/exercises/exercise_22.py
@@ -1,4 +1,21 @@
 class Coordinate:
+    """Coordinate class.
+
+    This class represents the coordinates (x, y) of a given point.
+
+    Attributes:_
+        x: the x coordinate of a point.
+        y: the y coordinate of a point.
+
+    """
+
     def __init__(self, x, y):
+        """Initialize a Coordinate instance.
+
+        Args:_
+            x: the x coordinate of the isntance.
+            y: the y coordinate of the instance.
+
+        """
         self.x = x
         self.y = y
--- lessons/classes/exercises/exercise_23.py
+++ lessons/classes/exercises/exercise_23.py
@@ -1,5 +1,25 @@
 class Car:
+    """Car class.
+
+    This class represents a car with 3 attributes (color, license plate and
+    vin number).
+
+    Attributes:_
+        color: the color of the car.
+        license plate: the license plate of the car.
+        vin number: the vin number of the car.
+
+    """
+
     def __init__(self, color, license_plate, vin_number):
+        """Initialize a Car instance.
+
+        Args:_
+            color: the color of the instance.
+            license_plate: the license plate of the instance.
+            vin_number: the vin number of the instance.
+
+        """
         self.color = color
         self._license_plate = license_plate
         self.__vin_number = vin_number
