e7641c008220169eeb99038c332bb85ebb763514
ChaoDavid20 <chaodavid20@gmail.com>
2023-11-04 18:20:54 +0100

    Update exercise_13.py
    Linters check

--- lessons/operators/exercises/exercise_13.py
+++ lessons/operators/exercises/exercise_13.py
@@ -1,3 +1,6 @@
 vector_1 = (4, 5.4213)  # Do not modify this line.
 vector_2 = (123, -412.21)  # Do not modify this line.
-distance = (((vector_1[0]-vector_2[0])**2)+((vector_1[1]-vector_2[1])**2))**0.5
+distance = (
+    (((vector_1[0] - vector_2[0])**2) +
+     ((vector_1[1] - vector_2[1])**2)) ** 0.5
+)
