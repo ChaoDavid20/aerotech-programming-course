280b20edbc34c9d76a069e1d6f4177091ce20a39
ChaoDavid20 <chaodavid20@gmail.com>
2023-11-04 20:02:26 +0100

    Update exercise_32.py
    Linters check

--- lessons/libraries/exercises/exercise_32.py
+++ lessons/libraries/exercises/exercise_32.py
@@ -2,6 +2,20 @@ import scipy
 
 
 def splitter():
+    """Split a RGB image in its three color channels.
+
+    This function splits a RGB image ins its three color channels. First it
+    imports an image. Then, it makes 3 copies of it. After, it isolates 1
+    differente channel in every copy. Finally, it returns a list with the 3
+    isolated channels.
+
+    Args:_
+        none
+
+    Returns_
+        [R, G, B]: the list with the 3 isolated channels.
+
+    """
     img = scipy.datasets.face()
     R = img.copy()
     G = img.copy()
